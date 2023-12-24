482b3c3589fe9ca2b12299aa61feb13a13afb59b
aaubrey06 <146406650+aaubrey06@users.noreply.github.com>
2023-12-24 09:20:40 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,38 @@
+
+/*--Log-in btn--*/
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+  }
+  
+  window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+      var dropdowns = document.getElementsByClassName("dropdown-content");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+  }
+
+  /*--Post Review--*/
+      const btn = document.querySelector("button");
+      const post = document.querySelector(".post");
+      const widget = document.querySelector(".star-widget");
+      const editBtn = document.querySelector(".edit");
+      btn.onclick = ()=>{
+        widget.style.display = "none";
+        post.style.display = "block";
+        editBtn.onclick = ()=>{
+          widget.style.display = "block";
+          post.style.display = "none";
+        }
+        return false;
+      }
+
+/*--Service Page (Heart)--*/
+document.getElementById('saveButton').addEventListener('click', function() {
+  this.classList.toggle('clicked');
+});
